8a57ebd37a5aa39a97bc2461dd63ca9f4695e23a
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-06 14:50:51 -0500

    New test class

---
+++ processtest.py
@@ -0,0 +1,10 @@
+import random
+import unittest
+
+class ProcessTest(unittest.TestCase) :
+
+    def test_placeholder(self) :
+        self.fail("PLACEHOLDER FOR TEST")
+
+if __name__ == '__main__' :
+    unittest.main()
