a01e68e255dbe7ac3b890636585bd1a81c0615fe
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-17 00:49:32 -0500

    Failing bopomofo are not kanji test

--- processtest.py
+++ processtest.py
@@ -47,5 +47,11 @@ class ProcessTest(unittest.TestCase) :
         message = f"Character '{char}' should not be kanji"
         assert not process.isKanji(char), message
         
+    def test_bopomofo_are_not_kanji(self) :
+        code = random.randint(0x3105, 0x312F)
+        char = chr(code)
+        message = f"Character '{char}' should not be kanji"
+        assert not process.isKanji(char), message
+        
 if __name__ == '__main__' :
     unittest.main()
