b0f230cbe899bf1ceb8827490789782837a3e408
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-01 01:16:43 -0500

    Failing test that circled numbers on black squares are not kanji

--- processtest.py
+++ processtest.py
@@ -91,5 +91,11 @@ class ProcessTest(unittest.TestCase) :
         message = f"Character '{char}' should be kanji"
         assert process.isKanji(char), message
         
+    def test_circled_numbers_on_black_squares_not_kanji(self) :
+        code = random.randint(0x3248, 0x324F)
+        char = chr(code)
+        message = f"Character '{char}' should not be kanji"
+        assert not process.isKanji(char), message
+        
 if __name__ == '__main__' :
     unittest.main()
