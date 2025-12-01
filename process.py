e7d31f6f1e3f57f511546cff4ce0b1c41fe56709
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-01 02:06:40 -0500

    Passing circled kanji are kanji test

--- process.py
+++ process.py
@@ -6,7 +6,7 @@ def isKanji(ch) -> bool :
         return False
     if ordinal > 0x321F and ordinal < 0x3248 :
         return True
-    return False # ordinal > 0x327F
+    return ordinal > 0x3280
 
 # TODO: Write tests for this
 def pickOutKanji(s: str) -> str :
