ce3016428ddf289d54c281fdb65f76d6aa76a5f7
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-01 00:58:12 -0500

    Refactored isKanji( )

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 # TODO: Write tests for this
 def isKanji(ch) -> bool :
-    return ord(ch) > 0x321E
+    return False # ord(ch) > 0x321E
 
 # TODO: Write tests for this
 def pickOutKanji(s: str) -> str :
