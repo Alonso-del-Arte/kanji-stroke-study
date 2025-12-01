87366ed21a492e2e42eecfd49b1e6f770a5f53ad
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-01 01:01:04 -0500

    Failing test that parenthesized kanji are kanji

--- processtest.py
+++ processtest.py
@@ -79,5 +79,11 @@ class ProcessTest(unittest.TestCase) :
         message = f"Character '{char}' should not be kanji"
         assert not process.isKanji(char), message
         
+    def test_parenthesized_kanji_are_kanji(self) :
+        code = random.randint(0x3220, 0x3243)
+        char = chr(code)
+        message = f"Character '{char}' should be kanji"
+        assert process.isKanji(char), message
+        
 if __name__ == '__main__' :
     unittest.main()
