b517ebff1ff0018760c6d443469b65e1d6cf0736
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-01 01:10:14 -0500

    Passing test that circled ARIB STD B24 kanji are kanji

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 # TODO: Write tests for this
 def isKanji(ch) -> bool :
     ordinal = ord(ch)
-    return ordinal > 0x321F and ordinal < 0x3244
+    return ordinal > 0x321F # and ordinal < 0x3244
 
 # TODO: Write tests for this
 def pickOutKanji(s: str) -> str :
