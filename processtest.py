ee242a0ee4ac1421f1d24efd83fb2b22e82ecf20
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-01 01:56:14 -0500

    Failing test that circled hangul syllables are not kanji

--- processtest.py
+++ processtest.py
@@ -114,5 +114,11 @@ class ProcessTest(unittest.TestCase) :
         message = f"Character '{char}' should not be kanji"
         assert not process.isKanji(char), message
         
+    def test_circled_hangul_syllables_are_not_kanji(self) :
+        code = random.randint(0x326E, 0x327B)
+        char = chr(code)
+        message = f"Character '{char}' should not be kanji"
+        assert not process.isKanji(char), message
+        
 if __name__ == '__main__' :
     unittest.main()
