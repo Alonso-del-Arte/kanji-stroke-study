046c46ce5c8565ef9daf381d3209518bcfaa61e4
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-01 23:52:02 -0500

    Passing telegraph month symbols are kanji test

--- process.py
+++ process.py
@@ -6,6 +6,8 @@ def isKanji(ch) -> bool :
         return False
     if ordinal > 0x321F and ordinal < 0x3248 :
         return True
+    if ordinal > 0x32BF and ordinal < 0x32CC :
+        return True
     return ordinal > 0x3280 and ordinal < 0x32B1
 
 # TODO: Write tests for this
