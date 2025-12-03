70117dc103a727419772812ab0c570e697620bca
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-03 13:10:04 -0500

    Passing Japanese era names are kanji tests

--- process.py
+++ process.py
@@ -18,7 +18,7 @@ def isKanji(ch) -> bool :
         return True
     if ordinal > 0x3357 and ordinal < 0x3371 :
         return True
-    return False
+    return ordinal > 0x337A and ordinal < 0x337F
 
 # TODO: Write tests for this
 def pickOutKanji(s: str) -> str :
