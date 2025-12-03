bcee99ebde6511ecd9b66af3eb4cdd309ffcc160
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-03 12:57:11 -0500

    Failing telegraph hour symbols are kanji test

--- processtest.py
+++ processtest.py
@@ -195,5 +195,11 @@ class ProcessTest(unittest.TestCase) :
         message = f"Character '{char}' should not be kanji"
         assert not process.isKanji(char), message
 
+    def test_telegraph_hour_symbols_are_kanji(self) :
+        code = random.randint(0x3358, 0x3370)
+        char = chr(code)
+        message = f"Character '{char}' should be kanji"
+        assert process.isKanji(char), message
+                
 if __name__ == '__main__' :
     unittest.main()
