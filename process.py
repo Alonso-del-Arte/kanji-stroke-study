ad87de2255e656ec99c73cec6609fe4534ee7c15
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-04 15:16:11 -0500

    Passing test to decompose telegraph symbols for hours

--- process.py
+++ process.py
@@ -20,14 +20,16 @@ def isKanji(ch) -> bool :
         return True
     return ordinal > 0x337A
 
-# TODO: Write tests for this
+# TODO: Write a lot more tests for this
 # The idea here is that this will take a character like U+337F, and decompose it 
 # to U+682A, U+5F0F, U+4F1A and U+793E.
 # To keep the scope manageable, I don't intend for this function to be used for 
 # characters in the CJK Unified Ideographs block or any of the lettered 
 # extension blocks.
 def decompose(ch) -> str :
-    return "SORRY, NOT IMPLEMENTED YET"
+    ordinal = ord(ch)
+    num_str = str((ordinal % 0x100) - 0x58)
+    return num_str + chr(0x70B9)
 
 # TODO: Write tests for this
 def pickOutKanji(s: str) -> str :
