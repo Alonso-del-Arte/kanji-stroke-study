8fd85a1a32d2e8aed0467750a71fc4cd8f5c2e59
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-04 15:25:55 -0500

    Passing square Latin abbreviations are not kanji test

--- process.py
+++ process.py
@@ -18,7 +18,9 @@ def isKanji(ch) -> bool :
         return True
     if ordinal > 0x3357 and ordinal < 0x3371 :
         return True
-    return ordinal > 0x337A
+    if ordinal > 0x337A and ordinal < 0x3380 :
+        return True
+    return ordinal > 0x3394
 
 # TODO: Write a lot more tests for this
 # The idea here is that this will take a character like U+337F, and decompose it 
