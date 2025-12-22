d90dfead079f43472bed0f47ed2eb85dd5711190
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-22 12:05:13 -0500

    Refactored decompose( ) to prioritize larger ranges of decomposables

--- process.py
+++ process.py
@@ -38,17 +38,17 @@ def isKanji(ch) -> bool :
 # extension blocks.
 def decompose(ch) -> str :
     ordinal = ord(ch)
-    if ordinal > 0x32BF and ordinal < 0x32CC :
-        num_str = str(ordinal - 0x32BF)
-        return num_str + chr(0x6708)
-    if ordinal > 0x3357 and ordinal < 0x3371 :
-        num_str = str((ordinal % 0x100) - 0x58)
-        return num_str + chr(0x70B9)
-    if ordinal == 0x32FF :
-        return chr(0x4EE4) + chr(0x548C)
     if ordinal > 0x33DF and ordinal < 0x33FF :
         num_str = str(ordinal - 0x33DF)
         return num_str + chr(0x65E5)
+    if ordinal > 0x3357 and ordinal < 0x3371 :
+        num_str = str((ordinal % 0x100) - 0x58)
+        return num_str + chr(0x70B9)
+    if ordinal > 0x32BF and ordinal < 0x32CC :
+        num_str = str(ordinal - 0x32BF)
+        return num_str + chr(0x6708)
+    if ordinal == 0x32FF :
+        return chr(0x4EE4) + chr(0x548C)
     if ordinal == 0x337B :
         return chr(0x5E73) + chr(0x6210)
     if ordinal == 0x337C :
