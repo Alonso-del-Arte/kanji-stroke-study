58eb42c69b1445c3544d65d0d68c4309541d6193
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-22 11:46:37 -0500

    Refactored decompose( )

--- process.py
+++ process.py
@@ -41,6 +41,9 @@ def decompose(ch) -> str :
     if ordinal > 0x32BF and ordinal < 0x32CC :
         num_str = str(ordinal - 0x32BF)
         return num_str + chr(0x6708)
+    if ordinal > 0x3357 and ordinal < 0x3370 :
+        num_str = str((ordinal % 0x100) - 0x58)
+        return num_str + chr(0x70B9)
     if ordinal == 0x32FF :
         return chr(0x4EE4) + chr(0x548C)
     if ordinal > 0x33DF and ordinal < 0x33FF :
